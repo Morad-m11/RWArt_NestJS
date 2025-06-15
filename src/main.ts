3c2f8ae630fcacb03f67eb2a3661a745dd03e6ba
Mo <Mohamed-m11@hotmail.com>
2025-06-15 21:13:55 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap(): Promise<void> {
+   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      abortOnError: false,
+   });
+
+   await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap().catch((err) => {
+   console.error(`Failed to start application. ${err}`);
+});
