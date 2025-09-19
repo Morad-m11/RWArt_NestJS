5654774fc726c3a139e3070e4069e7a6379154c3
Mo <80397392+Morad-m11@users.noreply.github.com>
2025-09-19 17:47:26 +0200

    feat(post-dto): added max length for title & description

--- src/modules/post/dto/create-post.dto.ts
+++ src/modules/post/dto/create-post.dto.ts
@@ -1,13 +1,15 @@
 import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreatePostDto {
     @IsString()
     @IsNotEmpty()
+    @MaxLength(100)
     title!: string;
 
     @IsString()
     @IsNotEmpty()
+    @MaxLength(200)
     description!: string;
 
     @Transform(({ value }) => toStringArray(value))
